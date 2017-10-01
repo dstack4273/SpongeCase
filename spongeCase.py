72c6eaca279760a3715c6d98ed9663a91122e446
dstack4273 <dstackmasta27@gmail.com>
2017-09-30 21:19:51 -0400

    initial commit for SpongeCase converter

---
+++ spongeCase.py
@@ -0,0 +1,44 @@
+import re
+feeder = str(input('Enter your string to convert to Sponge-Case! '))
+
+
+def spongecase(feedMe):
+    swapped = ""
+    looper = list(feedMe)
+    index = 0
+
+    for i in feedMe:
+
+        if index == 0:
+            if i.isupper():
+                # print("before increment " + str(index))
+                index += 1
+                # print("after increment " + str(index))
+                swapped += i.lower()
+                # print(swapped + " this is the " + str(index) + " loop")
+
+            else:
+                swapped += i.upper()
+                index += 1
+                # print(swapped + " this is the " + str(index) + " loop")
+        # elif swapped[index-1].isspace():
+        elif bool(re.match('[\W]+$', swapped[index-1])):
+            if swapped[index-2].isupper():
+                swapped += i.lower()
+                index += 1
+            else:
+                swapped += i.upper()
+                index += 1
+
+        elif swapped[index-1].isupper():
+            swapped += i.lower()
+            index += 1
+            # print(swapped + " this is the " + str(index) + " loop")
+        else:
+            swapped += i.upper()
+            index += 1
+            # print(swapped + " this is the " + str(index) + " loop")
+    return swapped
+
+
+print(spongecase(feeder))
